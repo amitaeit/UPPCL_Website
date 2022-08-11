e70466e7c5061a7a100eb16aba273a5586af81a9
Amit Yadav <amitaeit@gmail.com>
2022-08-11 10:53:40 +0530

    total consumption

--- IIITL_website_training/DOM_10-08-2022/AssignmentEval/bill.js
+++ IIITL_website_training/DOM_10-08-2022/AssignmentEval/bill.js
@@ -1,16 +1,27 @@
-usageDetails=[]
-function calculatebill(){
-    const numNode=document.querySelectorAll(".wattField");
-    let totwatt=0;
+usageDetails = []
+function calculatebill() {
+    const numNode = document.querySelectorAll(".wattField");
+    let totwatt = 0;
     // console.log(numNode);
     numNode.forEach(element => {
-        totwatt=totwatt + parseInt(element.value);
-        
+        totwatt = totwatt + parseInt(element.value);
+
     });
-    
-    document.querySelector(".billLoad").innerHTML= totwatt+" W";
-    const val=document.querySelectorAll('.appliance');
-    console.log(val.childNodes.length);
 
+    document.querySelector(".billLoad").innerHTML = totwatt + " W";
 
+    //calculate total consumption
+    const appliances = document.querySelectorAll('.appliance');
+    let totalconsumption = 0;
+    appliances.forEach(appliance => {
+        const wattageNode = appliance.querySelector(".wattField");
+        const usageNode = appliance.querySelector(".usageField");
+        totalconsumption += parseFloat(wattageNode.value)*parseFloat(usageNode.value)*30;
+    });
+
+    document.querySelector(".billAmount").innerHTML = totalconsumption +" KWH";
+}
+
+function deleteAppliance(this){
+    console.log(this);
 }
